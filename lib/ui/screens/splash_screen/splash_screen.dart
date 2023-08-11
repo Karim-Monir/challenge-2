108f359b822b5c2d0db3f41b9a54f4415fe85da2
Karim Monir <karim.monir1992@gmail.com>
2023-08-12 02:02:34 +0300

    smooth page indicator pkg added

--- lib/ui/screens/splash_screen/splash_screen.dart
+++ lib/ui/screens/splash_screen/splash_screen.dart
@@ -1,5 +1,9 @@
 import 'package:challenge_two/ui/screens/splash_screen/splash_pages/first_page.dart';
+import 'package:challenge_two/ui/screens/splash_screen/splash_pages/forth_page.dart';
+import 'package:challenge_two/ui/screens/splash_screen/splash_pages/second_page.dart';
+import 'package:challenge_two/ui/screens/splash_screen/splash_pages/third_page.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,12 +13,45 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final PageController _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: const [
-          FirstSplashPage(),
+      body: Stack(
+        children: [
+          PageView(
+            controller: _controller,
+            children: const [
+              FirstSplashPage(),
+              SecondSplashPage(),
+              ThirdSplashPage(),
+              ForthSplashPage(),
+            ],
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Next',
+              style: TextStyle(
+                color: Color(0xFF6CC51D),
+                fontSize: 15,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Container(
+              alignment: const Alignment(0.0, 0.75),
+              child: SmoothPageIndicator(
+                  controller: _controller,
+                  count: 4,
+                  effect: const JumpingDotEffect(
+                    dotColor: Color(0xFF797171),
+                    activeDotColor: Color(0xFF6CC51D),
+                    dotHeight: 8.0,
+                    dotWidth: 8.0,
+                    spacing: 4.0,
+                  )))
         ],
       ),
     );
